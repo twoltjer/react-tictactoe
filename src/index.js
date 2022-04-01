a17f1db5e0a151f338daf5b7437cf7e1607bd60d
Thomas Woltjer <Thomas.Woltjer@VibrationResearch.com>
2022-04-01 15:53:21 -0400

    Extended tic-tac-toe

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ComboBox from "react-widgets/ComboBox";
 import './index.scss';
-import "react-widgets/styles.css";
+import "react-widgets//styles.css";
 import x_small from './images/x_24x24.png';
 import o_small from './images/o_24x24.png';
 
@@ -12,7 +12,7 @@ function Square(props) {
     return (
       <button className="square" onClick={props.onClick} />
     );
-    }
+  }
 
   let image = props.value === 'X' ? x_small : o_small;
   return (
@@ -32,24 +32,30 @@ class Board extends React.Component {
     );
   }
 
+  renderRow(i) {
+    let squares = [];
+
+    for (let col = 0; col < this.props.boardSize; col++) {
+      squares.push(this.renderSquare(i * this.props.boardSize + col))
+    }
+
+    return (
+      <div className="board-row">
+        {squares}
+      </div>
+    );
+  }
+
   render() {
+    let rows = [];
+
+    for (let row = 0; row < this.props.boardSize; row++) {
+      rows.push(this.renderRow(row));
+    }
+
     return (
       <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+        {rows}
       </div>
     );
   }
@@ -65,7 +71,8 @@ class Game extends React.Component {
         }
       ],
       stepNumber: 0,
-      xIsNext: true
+      xIsNext: true,
+      boardSize: 3
     };
   }
 
@@ -86,6 +93,29 @@ class Game extends React.Component {
       stepNumber: history.length,
       xIsNext: !this.state.xIsNext
     });
+  }
+
+  handleBoardSizeChange(newSize) {
+    let size;
+    if (newSize === '3x3') {
+      size = 3;
+    } else if (newSize === '4x4') {
+      size = 4;
+    } else if (newSize === '5x5') {
+      size = 5;
+    } else {
+      alert("Invalid board size!");
+    }
+    this.setState({
+      boardSize: size,
+      history: [
+        {
+          squares: Array(size * size).fill(null)
+        }
+      ],
+      stepNumber: 0,
+      xIsNext: true,
+    })
   }
 
   jumpTo(step) {
@@ -124,13 +154,15 @@ class Game extends React.Component {
           <Board
             squares={current.squares}
             onClick={i => this.handleClick(i)}
+            boardSize={this.state.boardSize}
           />
         </div>
         <div className="game-info">
-          <ComboBox 
+          <ComboBox
             defaultValue="3x3"
             data={["3x3", "4x4", "5x5"]}
-            />
+            onChange={value => this.handleBoardSizeChange(value)}
+          />
           <div>{status}</div>
           <ol>{moves}</ol>
         </div>
@@ -144,20 +176,42 @@ class Game extends React.Component {
 ReactDOM.render(<Game />, document.getElementById("root"));
 
 function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-  ];
+  let sideLength = Math.sqrt(squares.length);
+  let lines = [];
+  // Diagonals are described as they would be understood if drawing the board from top to bottom
+  let leftToRightDiag = [];
+  let rightToLeftDiag = [];
+  for (let i = 0; i < sideLength; i++) {
+    var row = [];
+    var col = [];
+    for (let j = 0; j < sideLength; j++) {
+      // Row i will have indices of i * sideLength, i * sideLength + 1, etc
+      row.push(i * sideLength + j);
+      // Column i will have indices of i, i + sideLength, i + sideLength * 2, etc
+      col.push(i + j * sideLength);
+    }
+    lines.push(row);
+    lines.push(col);
+    // 5x5 grid left-to-right diag will be 0, 6, 12, 18, 24
+    leftToRightDiag.push(i * (sideLength + 1));
+    // 5x5 grid right-to-left diag will be 4, 8, 12, 16, 20
+    rightToLeftDiag.push((i + 1) * (sideLength - 1));
+  }
+  lines.push(leftToRightDiag);
+  lines.push(rightToLeftDiag);
+
   for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
+    let possibleWinner = squares[lines[i][0]];
+    if (possibleWinner) {
+      let isWinner = true;
+      for (let j = 1; j < sideLength; j++) {
+        if (!(squares[lines[i][j]] === possibleWinner)) {
+          isWinner = false;
+        }
+      }
+      if (isWinner) {
+        return possibleWinner;
+      }
     }
   }
   return null;
